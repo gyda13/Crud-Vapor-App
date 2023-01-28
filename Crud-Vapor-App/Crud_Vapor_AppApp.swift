c5e7ba003baf17f1bf16c7eb216fc9c66c4a2b67
gyda13 <gyda2100@gmail.com>
2023-01-29 01:31:46 +0300

    Initial Commit

---
+++ Crud-Vapor-App/Crud_Vapor_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crud_Vapor_AppApp.swift
+//  Crud-Vapor-App
+//
+//  Created by gyda almohaimeed on 07/07/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct Crud_Vapor_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
